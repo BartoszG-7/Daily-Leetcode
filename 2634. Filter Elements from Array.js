693ca230189829498ba38c8084f5cab7084fb478
BartoszG-7 <bapuch08@gmail.com>
2025-01-31 23:36:53 +0100

    2634. Filter Elements from Array (31.01.2025)
    Almost like the last problem but with an extra if statement filter. Pretty simple.

---
+++ 2634. Filter Elements from Array.js
@@ -0,0 +1,9 @@
+var filter = function(arr, fn) {
+    filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+};
