5750de0326512bb83482e6b6a4b811b0110127c7
BartoszG-7 <bapuch08@gmail.com>
2025-03-20 22:06:14 +0100

    1211. Queries Quality and Percentage (20.03.2025)
    I was playing around with this one, but it wasn't that bad. I enjoyed it.

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,1 @@
+SELECT query_name, ROUND(AVG(CAST(rating AS DECIMAL) / POSITION), 2) AS quality, ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage FROM queries GROUP BY query_name;
