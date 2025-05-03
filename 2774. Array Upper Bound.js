98bec2b9abbe21a235f573cba0c79c9eab467ee8
BartoszG-7 <bapuch08@gmail.com>
2025-05-03 23:57:37 +0200

    2774. Array Upper Bound (03.05.2025)
    Nothing too crazy here, it was fun.

---
+++ 2774. Array Upper Bound.js
@@ -0,0 +1,9 @@
+Array.prototype.upperBound = function(target) {
+    let result = -1;
+    this.forEach((val, ind)=> {
+        if(val === target){
+            result = ind;
+        }
+    })
+    return result;
+};
