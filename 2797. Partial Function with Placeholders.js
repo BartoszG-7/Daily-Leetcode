46d3d8ffcf56d8a83beb482d07a2f9c31201af78
Bartosz Gesina <bartoszg2008@gmail.com>
2025-05-13 23:50:05 +0200

    2797. Partial Function with Placeholders (13.05.2025)
    Pretty standard problem here, calm and enjoyable.

---
+++ 2797. Partial Function with Placeholders.js
@@ -0,0 +1,6 @@
+var partial = function(fn, args) {
+    return function(...restArgs) {
+        const replacedArgs = args.map(arg => arg === '_' ? restArgs.shift() : arg);
+        return fn(...replacedArgs.concat(restArgs));   
+    }
+};
