ce3b2fec2fd816be977a4d934c37926e94fe8a18
Bartosz Gesina <bartoszg2008@gmail.com>
2025-07-14 07:52:26 +0200

    263. Ugly Number (12.07.2025)
    Honestly a fun problem using division.

---
+++ 263. Ugly Number.js
@@ -0,0 +1,18 @@
+var isUgly = function(n) {    
+    if (n <= 0) {
+        return false;
+    }
+    
+    const keepDividingWhenDivisible = (dividend, divisor) => {
+        while (dividend % divisor == 0) {
+            dividend /= divisor;
+        }
+        return dividend;
+    }
+
+    for (const factor of [2, 3, 5]) {
+        n = keepDividingWhenDivisible(n, factor);
+    }
+
+    return n == 1;
+};
