19008320c86e500cb6f0fc91c5057e19c5a26b1b
BartoszG-7 <bapuch08@gmail.com>
2025-07-16 00:05:31 +0200

    168. Excel Sheet Column Title (16.07.2025)
    Interesting problem, actually kind of troublesome because you need some advanced knowledge for it.

---
+++ 168. Excel Sheet Column Title.js
@@ -0,0 +1,12 @@
+var convertToTitle = function (columnNumber) {
+    let ans = "";
+
+    while (columnNumber > 0) {
+        columnNumber--;
+        ans =
+            String.fromCharCode((columnNumber % 26) + "A".charCodeAt(0)) + ans;
+        columnNumber = Math.floor(columnNumber / 26);
+    }
+
+    return ans;
+};
