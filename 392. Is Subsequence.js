021cc555cf0f90fef35e45936fe889407d634ae5
BartoszG-7 <bapuch08@gmail.com>
2025-07-17 23:24:28 +0200

    392. Is Subsequence (17.07.2025)

---
+++ 392. Is Subsequence.js
@@ -0,0 +1,11 @@
+const isSubsequence = (s, t) => {
+  if (s.length > t.length) return false;
+  const t_length = t.length;
+  let subsequence = 0;
+  for (let i = 0; i < t_length; i++) {
+    if (s[subsequence] === t[i]) {
+      subsequence++;
+    }
+  }
+  return subsequence === s.length
+};
