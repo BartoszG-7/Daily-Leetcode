389bee1ee677a0ef649ed0bad880fea5b902a654
BartoszG-7 <bapuch08@gmail.com>
2025-07-27 23:13:00 +0200

    492. Construct the Rectangle (27.07.2025)

---
+++ 492. Construct the Rectangle.py
@@ -0,0 +1,6 @@
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        for l in range(int(area**0.5), 0, -1):            
+            if area % l == 0: 
+                return [area // l, l]
+            
