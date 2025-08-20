a1939764e73801f92a8c65acff80079ecbb25cb3
BartoszG-7 <bapuch08@gmail.com>
2025-08-20 21:45:53 +0200

    48. Rotate Image (20.08.2025)

---
+++ 48. Rotate Image.js
@@ -0,0 +1,18 @@
+var rotate = function (matrix) {
+    const n = matrix.length;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n / 2; j++) {
+            [matrix[i][j], matrix[i][n - 1 - j]] = [
+                matrix[i][n - 1 - j],
+                matrix[i][j],
+            ];
+        }
+    }
+};
