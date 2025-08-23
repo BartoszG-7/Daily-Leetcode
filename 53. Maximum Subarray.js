e93fbb21e2f83b6e815031d17eee3b7b4d281e01
BartoszG-7 <bapuch08@gmail.com>
2025-08-23 23:31:32 +0200

    53. Maximum Subarray (23.08.2025)

---
+++ 53. Maximum Subarray.js
@@ -0,0 +1,10 @@
+var maxSubArray = function (nums) {
+    let currentSubarray = nums[0];
+    let maxSubarray = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        let num = nums[i];
+        currentSubarray = Math.max(num, currentSubarray + num);
+        maxSubarray = Math.max(maxSubarray, currentSubarray);
+    }
+    return maxSubarray;
+};
