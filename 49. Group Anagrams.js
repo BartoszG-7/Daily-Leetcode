671e63a1d6db16c1c331a03dbdb0e91bbaa6534f
BartoszG-7 <bapuch08@gmail.com>
2025-08-23 23:17:49 +0200

    49. Group Anagrams (21.08.2025)

---
+++ 49. Group Anagrams.js
@@ -0,0 +1,11 @@
+var groupAnagrams = function (strs) {
+    let map = new Map();
+    for (let str of strs) {
+        let chars = Array.from(str);
+        chars.sort();
+        let key = chars.join("");
+        if (!map.has(key)) map.set(key, []);
+        map.get(key).push(str);
+    }
+    return Array.from(map.values());
+};
