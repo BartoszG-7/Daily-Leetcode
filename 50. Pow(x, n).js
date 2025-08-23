dea00198dcaa7aafbdab0e476a95a08c367e5fd4
BartoszG-7 <bapuch08@gmail.com>
2025-08-23 23:30:00 +0200

    50. Pow(x, n) (22.08.2025)

---
+++ 50. Pow(x, n).js
@@ -0,0 +1,22 @@
+function binaryExp(x, n) {
+
+    if (n === 0) {
+        return 1;
+    }
+
+    if (n < 0) {
+        return 1 / binaryExp(x, -1 * n);
+    }
+
+    if (n % 2 === 1) {
+        return x * binaryExp(x * x, (n - 1) / 2);
+    }
+
+    else {
+        return binaryExp(x * x, n / 2);
+    }
+}
+
+var myPow = function (x, n) {
+    return binaryExp(x, n);
+};
